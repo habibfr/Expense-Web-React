fb17ccb8e2beab334bb8d1836a2fc169a7f63415
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2022-02-11 21:36:07 +0700

    cek jika data kosong

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,21 +5,51 @@ import "./Expenses.css";
 import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
-  const [filteredYear, setSelectYear] = useState("");
+  const [filteredYear, setSelectYear] = useState("All");
 
+  // handle filter change by year
   const filterChangeHandler = (selectedYear) => {
     setSelectYear(selectedYear);
   };
 
-  return (
-    <div>
-      <Card className="expenses">
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
+  // filter expenses by year
+  const filteredExpenses = props.items.filter((expense) => {
+    return (
+      expense.date.getFullYear().toString() === filteredYear ||
+      filteredYear === "All"
+    );
+  });
 
-        {props.items.map((expense) => {
+  // check if there is any expense
+  let expenseList = <p>No Expenses</p>;
+  if (filteredExpenses.length > 0) {
+    expenseList = filteredExpenses.map((expense) => {
+      return (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amount={expense.amount}
+          date={expense.date}
+        />
+      );
+    });
+  }
+
+  return (
+    <Card className="expenses">
+      <ExpensesFilter
+        selected={filteredYear}
+        onChangeFilter={filterChangeHandler}
+      />
+      {/* {props.items
+        .filter((expense) => {
+          if (filteredYear === "All") {
+            return true;
+          } else {
+            return expense.date.getFullYear() === parseInt(filteredYear);
+          }
+        })
+        .map((expense) => {
           return (
             <ExpenseItem
               key={expense.id}
@@ -28,9 +58,11 @@ function Expenses(props) {
               date={expense.date}
             />
           );
-        })}
-      </Card>
-    </div>
+        })} */}
+      {/* {filteredExpenses.length === 0 && <h3> Kosong Bro </h3>} */}
+      {/* {filteredExpenses.length > 0 &&} */}
+      {expenseList}
+    </Card>
   );
 }
 
